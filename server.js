845cf9a0742a44abb41694034c36c3742fd58ab2
kvk745 <kvk745@my.utsa.edu>
2021-04-13 16:33:10 -0500

    ordered the route getters

--- server.js
+++ server.js
@@ -3,22 +3,15 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if(process.env.NODE_ENV === 'production') {  
-  app.use(express.static(path.join(__dirname, 'client/build')));  
-  app.get('*', (req, res) => {    
-    res.sendFile(path.join(__dirname = 'client/build/index.html'));  }
-  )
-}
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-app.get('*', (req, res) => {  
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
+app.get('*', (req, res) => {    
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));  
+});
 
 app.listen(port, (req, res) => {  console.log( `Server listening on port: ${port}`);})
 
