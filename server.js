829833b6e1ad3987bffe0814599e42c1073d8337
kvk745 <kvk745@my.utsa.edu>
2021-04-13 15:54:05 -0500

    added server code to server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// create a GET route
+if(process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, 'client/build')));  
+  app.get('*', (req, res) => {    
+    res.sendFile(path.join(__dirname = 'client/build/index.html'));  }
+  )
+}
+
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.get('*', (req, res) => {  
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+app.listen(port, (req, res) => {  console.log( `Server listening on port: ${port}`);})
+
