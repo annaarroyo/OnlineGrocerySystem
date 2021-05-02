f1ee8c76b55592185c726b6f9cab7f593beff997
kvk745 <kvk745@my.utsa.edu>
2021-05-01 22:19:47 -0500

    updated router for Products and Home

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import React, { Component } from 'react';
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "./components/Pages/Home";
-import  Login  from "./components/Pages/login/Login";
+import Home from "./components/Pages/Home";
+import Login from "./components/Pages/login/Login";
+import Products from "./components/Pages/Products";
 class App extends Component {
 state = {
     data: null
@@ -15,16 +16,6 @@ state = {
       .then(res => this.setState({ data: res.express }))
       .catch(err => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
 
   render() {
     return (
@@ -34,8 +25,9 @@ state = {
 
               <div className="pages">
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <Route exact path="/"> <Home /> </Route>
                   <Route exact path="/login"> <Login /> </Route>
+                  <Route exact path="/search"> <Products /> </Route>
                 </Switch>
               </div>
             </Router>
