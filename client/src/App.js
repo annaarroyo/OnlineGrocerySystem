4835beea8a2789c000f9ea16e1713a5353c0ae6c
Anna Flores <annaflowersk@gmail.com>
2021-05-05 22:00:35 -0500

    Added Cart page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Pages/Home";
 import Login from "./components/Pages/Login";
 import Shop from "./components/Pages/Shop";
 import Profile from "./components/Pages/Profile";
+import Cart from "./components/Pages/Cart"
 import {DataProvider} from './components/Context'
 
 class App extends Component {
@@ -43,6 +44,7 @@ state = {
                   <Route exact path="/login"> <Login /> </Route>
                   <Route exact path="/search"> <Shop /> </Route>
                   <Route exact path="/profile"> <Profile /> </Route>
+                  <Route exact path="/cart"> <Cart /> </Route>
                 </Switch>
               </div>
             </Router>
